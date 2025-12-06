98f41afc092b09b0942b6407d368367ca6c72946
이대성 <dlkdm0703@gmail.com>
2025-12-07 01:46:50 +0900

    테스트 DB를 분리하지 않고, .env 파일 통합

--- src/config/loadEnv.js
+++ src/config/loadEnv.js
@@ -1,9 +1,4 @@
 ﻿const path = require('path');
 const dotenv = require('dotenv');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const envFile = nodeEnv === 'test' ? '.env.test' : '.env';
-
-dotenv.config({ path: path.resolve(process.cwd(), envFile) });
-
-module.exports = { nodeEnv };
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
